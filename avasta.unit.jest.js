751f1bcc3ca87870a643834c329758aaa8cb512e
bguiz <github@bguiz.com>
2017-04-03 23:06:26 +0800

    =BG= feat(schwag): test + impl schwag responses

--- avasta.unit.jest.js
+++ avasta.unit.jest.js
@@ -26,28 +26,54 @@ describe('[avasta]', () => {
 		});
 	});
 
-	it('handle name and message', () => {
-		testAvasta.expressJsonResponse(res, '1st 400 error', 'custom message');
-		expect({
-			status: resStatus.mock.calls[resStatus.mock.calls.length - 1][0],
-			json: resJson.mock.calls[resJson.mock.calls.length - 1][0],
-		}).toMatchSnapshot();
+	describe('[express]', () => {
+
+		it('handle name and message', () => {
+			testAvasta.expressJsonResponse(res, '1st 400 error', 'custom message');
+			expect({
+				status: resStatus.mock.calls[resStatus.mock.calls.length - 1][0],
+				json: resJson.mock.calls[resJson.mock.calls.length - 1][0],
+			}).toMatchSnapshot();
+		});
+
+		it('handle name without message', () => {
+			testAvasta.expressJsonResponse(res, '1st 400 error');
+			expect({
+				status: resStatus.mock.calls[resStatus.mock.calls.length - 1][0],
+				json: resJson.mock.calls[resJson.mock.calls.length - 1][0],
+			}).toMatchSnapshot();
+		});
+
+		it('handle undefined name', () => {
+			testAvasta.expressJsonResponse(res, 'this is not a known name');
+			expect({
+				status: resStatus.mock.calls[resStatus.mock.calls.length - 1][0],
+				json: resJson.mock.calls[resJson.mock.calls.length - 1][0],
+			}).toMatchSnapshot();
+		});
+
 	});
 
-	it('handle name without message', () => {
-		testAvasta.expressJsonResponse(res, '1st 400 error');
-		expect({
-			status: resStatus.mock.calls[resStatus.mock.calls.length - 1][0],
-			json: resJson.mock.calls[resJson.mock.calls.length - 1][0],
-		}).toMatchSnapshot();
-	});
+	describe('[schwag]', () => {
 
-	it('handle undefined name', () => {
-		testAvasta.expressJsonResponse(res, 'this is not a known name');
-		expect({
-			status: resStatus.mock.calls[resStatus.mock.calls.length - 1][0],
-			json: resJson.mock.calls[resJson.mock.calls.length - 1][0],
-		}).toMatchSnapshot();
+		it('handle name and message', () => {
+			const output = {};
+			testAvasta.schwagResponse(output, '1st 400 error', 'custom message');
+			expect(output).toMatchSnapshot();
+		});
+
+		it('handle name without message', () => {
+			const output = {};
+			testAvasta.schwagResponse(output, '1st 400 error');
+			expect(output).toMatchSnapshot();
+		});
+
+		it('handle undefined name', () => {
+			const output = {};
+			testAvasta.schwagResponse(output, 'this is not a known name');
+			expect(output).toMatchSnapshot();
+		});
+
 	});
 
 });
